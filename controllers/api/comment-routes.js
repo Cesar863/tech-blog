b501c6e1083e74a8c7791355cb85a372284c1555
Cesar863 <90540881+Cesar863@users.noreply.github.com>
2022-02-21 21:04:11 -0500

    fixed heroku issue

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const {
     Comment
-} = require("../../models/");
+} = require("../../models");
 const withAuth = require("../../utils/auth");
 
 router.post("/", withAuth, (req, res) => {
